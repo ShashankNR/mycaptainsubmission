30bf5f36c0ad77e4c5e219dd9efbcd23653d5faa
ShashankNR <66787071+ShashankNR@users.noreply.github.com>
2020-06-11 19:45:17 +0530

    Swap.c
    The output of the program displays the original and swapped numbers.

--- swap.c
+++ swap.c
@@ -7,9 +7,10 @@ int main()
 	printf("Before swapping\n");
 	printf("a=%d\n",a);
 	printf("b=%d\n",b);
-	a=a+b; // a=12
-	b=a-b; // b=4
-	a=a-b; // a=8
+	a=a+b; // a=12 because it's added with b
+	b=a-b; // b=4 since we need to decrease it by changed a again for
+               // it to return to the value of a.
+	a=a-b; // a=8 it's decreased with b whose value is now 4.
 	printf("After swapping\n");
 	printf("a=%d\n",a);
 	printf("b=%d\n",b);
