c034b1c6a71a9eb9381952b342a96dbd97705dac
ShashankNR <66787071+ShashankNR@users.noreply.github.com>
2020-06-13 14:10:46 +0530

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	// 1. Display their menu
+        // 1. Display their menu
 	printf("Hello Byte! Enter a random number from 1 to 5: \n");
 	// 2. Read their choice
 	int choice=0;
@@ -9,12 +9,12 @@ int main()
 	// 3. Display based one thier choice
 	switch(choice)
 	{
-		case 1:
-			printf("Food item - French Fries\nPrice - Rs 99");
-			break;
-		case 2:
-			printf("Food item - Sandwich\nPrice - Rs 149");
-			break;
+	    case 1:
+		printf("Food item - French Fries\nPrice - Rs 99");
+		break;
+	    case 2:
+		printf("Food item - Sandwich\nPrice - Rs 149");
+		break;
 	    case 3:
 	    	printf("Food item - Pasta\nPrice - Rs 179");
 	    	break;
@@ -23,9 +23,9 @@ int main()
 	    	break;
 	    case 5:
 	    	printf("Food item - Pizza\nPrice - Rs 239");
-			break;
-		default:
-		    printf("Sorry Byte you didn't choose anything between 1 to 5");	    	
+		break;
+	    default:
+		printf("Sorry Byte you didn't choose anything between 1 to 5");	    	
 	}
 	return 0;
 }
